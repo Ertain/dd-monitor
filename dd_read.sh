de26b96734dcb4be70054526d49a989d6b469117
Jason Anderson <sirius-c@iname.com>
2016-08-27 14:02:23 -0500

    Added license and minor improvements.

--- dd_read.sh
+++ dd_read.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # This script is mainly meant for making an image of
 # SD cards.
-# Usage: dd_read.sh /dev/foo output_image.img
+# Usage: dd_read.sh /dev/foo output_sd_card_image.img
 
 function usage {
 	echo "Usage:"
-	echo "Create an image from a block device while \
-	utilizing the pretty progress bar from pv."
+	echo "Create an image from a block device while utilizing the pretty progress bar from pv."
 	echo "dd_write.sh /dev/foo /Your/disc/image"
 	}
 
 if [ "$(id -u)" -ne 0 ]; then
   echo "Please run this script as root."
+  usage
   exit 1
 fi
 
@@ -23,16 +36,22 @@ if [ $# -ne 2 ]; then
 fi
 
 # Check to see whether they have pv installed.
-[ ! -f /usr/bin/pv ] && echo "Please install pv (hint: try using \
- apt-get install pv)." && exit 3
+if [ ! $(which pv) ]; then
+  echo "Please install pv (hint: try using \
+  apt-get install pv)."
+  usage
+  exit 3
+fi
  
 if [ ! -b "$1" ]; then
   echo "Please pass a valid block device."
+  usage
   exit 4
 fi
 
+# TODO Is this check really necessary?
 if [ ! -f "$2" ]; then
-  echo "File not found! Creating file..."
+  echo "File not found! Creating file."
   touch "$2"
 fi
 
@@ -47,7 +66,7 @@ echo "Now copying..."
 
 dd if="$1" | pv -s "${file_size}k" | dd of="$2" bs=4096
 
-echo "Okay, all done."
+echo "Okay, all copied."
 echo "Goodbye!"
 
 exit 0
--- dd_write.sh
+++ dd_write.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # This is a little script for writing disk images
 # to block devices, mainly SD cards.
+# Usage: dd_write.sh /dev/foo input_sd_card_image.img
+
 
 function usage {
 	echo "Usage:"
@@ -36,7 +51,7 @@ if [ ! -b "$1" ]; then
   exit 3
 fi
 
-# This should be changed to include a check for
+# TODO This should be changed to include a check for
 # whether the file is a disc image.
 if [ -f "$2" ]; then
     file_size=$(du -sb "$2" | awk '{print $1}')
